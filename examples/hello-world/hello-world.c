3fae629650c96805f4d720847b22318b31b65e21
Lyudmil Popov <lyud96@hotmail.co.uk>
2019-01-23 14:14:17 +0000

    implementing flash LED function

--- examples/hello-world/hello-world.c
+++ examples/hello-world/hello-world.c
@@ -31,63 +31,143 @@
  */
 
 /**
- * \file
- *         A very simple Contiki application showing how Contiki programs look
- * \author
- *         Adam Dunkels <adam@sics.se>
+ 	PROGRAM DESC..
  */
 
 #include "contiki.h"
 #include "dev/leds.h"
+#include "dev/button-hal.h"
 #include "sys/etimer.h"
 
-#include <stdio.h> /* For printf() */
-/*---------------------------------------------------------------------------*/
-static struct etimer et;
-/*---------------------------------------------------------------------------*/
-PROCESS(hello_world_process, "Hello world process");
-AUTOSTART_PROCESSES(&hello_world_process);
-/*---------------------------------------------------------------------------*/
-PROCESS_THREAD(hello_world_process, ev, data)
-{
+#include <stdio.h>
 
+/* Magic number RGB definitions*/
+#define RED 1
+#define GREEN 20
+#define BLUE 30
+#define REDGREEN 21
+#define REDBLUE 31
+#define GREENBLUE 50
+
+/*---------------------------------------------------------------------------*/
+PROCESS(led_sequence, "LED sequence process");
+PROCESS(btn_press, "Button press LED process");
+AUTOSTART_PROCESSES(&led_sequence, &btn_press);
+/*---------------------------------------------------------------------------*/
+void process_sequence(int a[][2], int alen){
+	
+	/* Variables declared locally to function instead of loop for readability.
+	Efficiency is equal.*/
+	int colour;
+	int time;
+	
+	/* Print table for testing purposes */
+	for ( int i = 0; i < alen; i++ ){
+ 		for (  int j = 0; j < 2; j++ ){
+    	printf("a[%d][%d] = %d\n", i,j, a[i][j] );
+  	}
+  }
+  
+  /* Loop through inner elements and store in variables */ 
+	for(int i = 0; i < alen; i++){
+		for(int j = 0; j < 1; j++){
+			colour = a[i][j];
+			time =  a[i][j+1];
+			flash_sequence(colour,time);
+			/* Print and compare with table for testing */
+			printf("colour: %d time: %d\n",colour,time);
+		}
+	}
+	
+}
+
+void flash_sequence(int colour, int time){
+	switch(colour) {
+		//Red	
+		case 1:
+		 		printf("RED for %d seconds",time);
+		    break; 
+		 //Green
+		 case 20:
+		 		printf("GREEN for %d seconds",time);
+		    break; 
+		 //Blue   
+		 case 30:
+		 		printf("BLUE for %d seconds",time);
+		    break; 
+		 //Red & Green   
+		 case 21:
+		 		printf("RED & GREEN for %d seconds",time);
+		    break;    
+		 //Red & Blue   
+		 case 31:
+		 		printf("RED & BLUE for %d seconds",time);
+		    break;   
+			//Green & Blue
+			case 50:
+		 		printf("GREEN & BLUE for %d seconds",time);
+		    break; 
+		    
+		 	default :
+		 		printf("No colour input");
+	}
+}
+/*---------------------------------------------------------------------------*/
+PROCESS_THREAD(led_sequence, ev, data)
+{
+	static struct etimer et1;
+  static struct etimer et2;
+  
+  /* Declare multidimensional array with sequences */
+  int sequence[][2] = { {RED,2}, {BLUE,4}, {REDBLUE,1}, {GREEN,1}, {GREENBLUE,1},
+  {REDGREEN,1}, {BLUE,21} };
+  
+  /* Compute amount of sequence inner arrays as cant be done in function */
+  int sequence_len = sizeof(sequence)/sizeof(sequence[0]);
   PROCESS_BEGIN();
 	
 	printf("Lyudmil Popov\n");	
 	
-	while(1){
+	/* Call function with sequence and length arguments */
+	process_sequence(sequence, sequence_len);
 	
-		leds_on(LEDS_RED);
-		printf("red LED: ON \n");
+	etimer_set(&et1, CLOCK_SECOND);
+	etimer_set(&et2, CLOCK_SECOND * 3);
+	
+	while(1) {
+		PROCESS_YIELD();
 		
-		etimer_set(&et, CLOCK_SECOND * 2);
-		PROCESS_WAIT_EVENT();
-		if(etimer_expired(&et)) {
+		if(ev == button_hal_press_event){
+			PROCESS_WAIT_EVENT_UNTIL(ev == button_hal_release_event);	
+		}
+		if(etimer_expired(&et1)){
 			leds_off(LEDS_RED);
-			printf("red LED: OFF\n");
-			leds_on(LEDS_BLUE);
-			printf("blue LED: ON\n");
+			printf("etimer expired\n");
+			etimer_reset(&et1);
 		}
-		
-		etimer_set(&et, CLOCK_SECOND * 4);
-		PROCESS_WAIT_EVENT();
-		
-		if(etimer_expired(&et)) {
+		if(etimer_expired(&et2)){
 			leds_on(LEDS_RED);
-			printf("DOUBLE\n");
-		}
-		
-		etimer_set(&et, CLOCK_SECOND);
-		
-		PROCESS_WAIT_EVENT();
-		
-		if(etimer_expired(&et)) {
-			
-			leds_off(LEDS_ALL);
-			printf("red LED: OFF\n");
-			printf("blue LED: OFF\n");
+			printf("red\n");
+			etimer_reset(&et2);
 		}
 	}
+		
+  PROCESS_END();
+}
+/*---------------------------------------------------------------------------*/
+PROCESS_THREAD(btn_press, ev, data)
+{
+
+  PROCESS_BEGIN();
+  while(1){
+		PROCESS_YIELD();
+  	if(ev == button_hal_press_event){
+			leds_off(LEDS_RED);
+			leds_on(LEDS_GREEN);		
+		}else if(ev == button_hal_release_event){
+			leds_off(LEDS_GREEN);
+		}
+  }
   PROCESS_END();
 }
 /*---------------------------------------------------------------------------*/
